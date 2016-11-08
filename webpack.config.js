8427d11049ec8bf8a827e6f37bf2c802fdcfc29f
wlad-tk <vlad.tkachenko@smoozeedev.com>
2016-11-08 12:47:03 +0200

    add plugins optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,69 @@
 var webpack = require('webpack');
+var BundleTracker  = require('webpack-bundle-tracker');
+
+var PROD = JSON.parse(process.env.PROD_ENV || '0');
+
+//console.log('Building with entry point: ' + ENTRY);
+//console.log('Production mode: ' + PROD);
+//console.log(' ');
 
 module.exports = {
     entry: "./src/main.js",
     output: {
         path: __dirname + '/public/build/',
         publicPath: "build/",
-        filename: "bundle.js"
+        filename: PROD ? 'b.min.js' : 'b.js' //'bundle.min.js' or 'bundle.js'
     },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            compress: {
-                warnings: false
+    devtool: 'source-map',
+    plugins: PROD ? [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify('production')
             }
         }),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            sourceMap: false,
+            output: {
+                comments: false
+            },
+            compress: {
+                warnings: true
+            }
+        }),
+        new BundleTracker({filename: './public/build/webpack-stats.json'})
+    ] : [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+            }
+        }),
+        new BundleTracker({filename: './public/build/webpack-stats.json'})
     ],
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx?)$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel'
+                loader: 'babel-loader',
+                query: {
+                    presets:['es2015', 'react']
+                }
             },
             {
-                test: /\.css$/,
-                loader: "style-loader!css-loader!postcss-loader",
-                exclude: [/node_modules/, /public/]
-            },
-            {
-                test: /\.less$/,
+                test: /\.(css|less)$/,
                 loader: "style-loader!css-loader!postcss-loader!less",
                 exclude: [/node_modules/, /public/]
             },
             {
-                test: /\.gif$/,
-                loader: "url-loader?limit=10000&mimetype=image/gif"
-            },
-            {
-                test: /\.jpg$/,
-                loader: "url-loader?limit=10000&mimetype=image/jpg"
-            },
-            {
-                test: /\.png$/,
-                loader: "url-loader?limit=10000&mimetype=image/png"
-            },
-            {
-                test: /\.svg/,
-                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
-            },
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                    presets:['es2015', 'react']
-                }
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&name=image/[hash].[ext]'
+                ]
             },
             {
                 test: /\.json$/,
